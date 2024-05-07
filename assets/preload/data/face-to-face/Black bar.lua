69ef5bb439e904dfc085735a1bbb351c8b17642b
TheZard <117235821+HeitorzinKkkj@users.noreply.github.com>
2024-05-07 13:49:54 -0300

    Add files via upload

---
+++ assets/preload/data/face-to-face/Black bar.lua
@@ -0,0 +1,9 @@
+function onCreate()
+    
+    makeLuaSprite('black Hud', 'black Hud', -100, 0);
+	scaleObject('black Hud', 0.6, 0.6);
+	setObjectCamera('black Hud', 'hud');
+	addLuaSprite('black Hud', true);
+	
+
+end	
